992750e6bddb8a201b360baba330d768e7f74045
test <1345491322@qq.com>
2022-03-06 10:04:24 +0800

    clean file

--- src/faker_software/__init__.py
+++ src/faker_software/__init__.py
@@ -3,18 +3,12 @@
 from random import choice
 from faker.providers import BaseProvider
 from operating_system_dict import os_names
-from faker import Faker
 from random import randint
 
 
 class OperatingSystemProvider(BaseProvider):
     """
     A Provider for operating system related test data
-
-    # >>> from faker import Faker
-    # >>> from faker_software import OperatingSystemProvider
-    # >>> fake = Faker()
-    # >>> fake.add_provider(OperatingSystemProvider)
 
     """
     def os_name(self):
--- src/faker_software/parser_wiki.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-r = requests.get('https://en.wikipedia.org/wiki/List_of_operating_systems', timeout=10)
-
-
-print(r)
-
